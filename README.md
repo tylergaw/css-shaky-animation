9fed7d40c44cdcb0a1be2d7404f0e28aeed57ef0
Tyler Gaw <me@tylergaw.com>
2012-09-01 20:57:59 -0400

    Adding all the work

---
+++ README.md
@@ -0,0 +1,8 @@
+# CSS Shaky Animation
+
+A master drawing is traced quickly which makes each frame slightly different
+and gives the shaky effect.
+
+Each title has three frames. The images are svg so they scale all pretty.
+
+[http://lab.tylergaw.com/css-shaky-animation](http://lab.tylergaw.com/css-shaky-animation/)
---
+++ css/config.rb
@@ -0,0 +1,21 @@
+# This is a compass (sass/stylesheet) configuration file.
+#
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+css_dir = "."
+sass_dir = "./scss"
+
+# Images
+images_dir = "../images"
+
+# Output
+output_style = :expanded
+line_comments = true
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+preferred_syntax = :scss
+
+asset_cache_buster :none
